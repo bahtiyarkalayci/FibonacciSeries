c5afbf9850953d8509f230a55e5349de0e9dc330
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-15 14:51:16 +0300

    https://www.patika.dev/tr

--- src/FibonacciSeries.java
+++ src/FibonacciSeries.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class FibonacciSeries {
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int number;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter the fibonacci's number");
+        number=scanner.nextInt();
+        int total=0;
+        int n1=0,n2=1,n3;
+        for (int i=0 ;i<=number;i++){
+
+            n3=n1+n2;
+            System.out.print(n1+" ");
+            n1=n2;
+            n2=n3;
+
+        }
+    }
 }
